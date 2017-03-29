2c40c7ba76602cca89123483a8d904807d891ed9
Martin Stabe <martinstabe@gmail.com>
2017-03-29 14:17:57 +0100

    set Onward Journey back to 2 rows

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,5 +1,5 @@
 export default () => ({
   relatedContent: [
-    { rows: 1, list: 'thing/NDdiMzAyNzctMTRlMy00Zjk1LWEyZjYtYmYwZWIwYWU2NzAy-VG9waWNz' },
+    { rows: 2, list: 'thing/NDdiMzAyNzctMTRlMy00Zjk1LWEyZjYtYmYwZWIwYWU2NzAy-VG9waWNz' },
   ],
 });
